b9cd41ae264de60e3bab1029c8203b867808afc4
Ann-Mahoney <stefanymahoney12@gmail.com>
2025-07-07 13:12:59 +0000

    change database.php to a class called Database for encapsulation

--- database.php
+++ database.php
@@ -1,16 +1,38 @@
 <?php
 // database.php - PDO database connection
 
-$host = 'localhost'; // Change if needed
-$dbname = 'car_rentals'; // Change to your database name
-$username = 'root'; // Change to your database username
-$password = ''; // Change to your database password
+// $host = 'localhost'; // Change if needed
+// $dbname = 'car_rentals'; // Change to your database name
+// $username = 'root'; // Change to your database username
+// $password = ''; // Change to your database password
 
-try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    // Set error mode to exceptions
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Database connection failed: " . $e->getMessage());
+class Database{
+    private $db;
+    private $hostname;
+    private $username;
+    private $password;
+    private $database;
+
+    public function __construct($hostname, $username, $password, $database){
+        $this->hostname = $hostname;
+        $this->username = $username;
+        $this->password = $password;
+        $this->database = $database;
+    }
+
+    public function connect(){
+        $dsn = "mysql:host={$this->hostname};dbname={$this->database}";
+        $this->db= new PDO($dsn, $this->username, $this->password);
+        $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $this->db;
+    }
 }
+
+// try {
+//     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+//     // Set error mode to exceptions
+//     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (PDOException $e) {
+//     die("Database connection failed: " . $e->getMessage());
+// }
 ?>
