b8624049d12c0e168597073bb02545b9dd79cdad
Ann-Mahoney <stefanymahoney12@gmail.com>
2025-07-08 08:40:17 +0000

    add binding

--- database.php
+++ database.php
@@ -18,13 +18,38 @@ class Database{
         $this->username = $username;
         $this->password = $password;
         $this->database = $database;
+        $this->connect();
     }
 
     public function connect(){
+        try{
         $dsn = "mysql:host={$this->hostname};dbname={$this->database}";
         $this->db= new PDO($dsn, $this->username, $this->password);
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $this->db;
+        }catch(PDOException $e){
+            echo "server down". $e->getMessage();
+        }
+    }
+
+    public function storeCarRental($name, $email, $contact, $pickup, $return_date, $offer, $comment){
+       try{
+         $query = $this->db->prepare("INSERT INTO `booking`( `name`, `email`, `offer`, `contact`, `pickup`, `return_date`, `comment`)
+         VALUES (?,?,?,?,?,?,?)");
+
+         $query->bindParam(1, $name);
+         $query->bindParam(2, $email);
+         $query->bindParam(3, $contact);
+         $query->bindParam(4, $pickup);
+         $query->bindParam(5, $return_date);
+         $query->bindParam(6, $offer);
+         $query->bindParam(7, $comment);
+
+         $query->execute();
+         return true;
+       }catch(PDOException $e){
+        echo "failed to insert into the table". $e->getMessage();
+       }
     }
 }
 
